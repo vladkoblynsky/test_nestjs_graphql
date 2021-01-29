776dcc6fe0cb7e093535b8e6520a76512fc933ea
vladorton <vladkoblinskyi@gmail.com>
2021-01-29 15:13:24 +0300

    Add function to generate schema

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,28 @@
 import { NestFactory } from '@nestjs/core';
+import {
+  GraphQLSchemaBuilderModule,
+  GraphQLSchemaFactory,
+} from '@nestjs/graphql';
+import * as fs from 'fs';
+import { printSchema } from 'graphql';
 import { AppModule } from './app.module';
+import { AuthorsResolver } from './authors/authors.resolver';
+import { PostsResolver } from './posts/posts.resolver';
+
+// async function generateSchema() {
+//   const app = await NestFactory.create(GraphQLSchemaBuilderModule);
+//   await app.init();
+
+//   const gqlSchemaFactory = app.get(GraphQLSchemaFactory);
+//   const schema = await gqlSchemaFactory.create([
+//     AuthorsResolver,
+//     PostsResolver,
+//   ]);
+//   fs.writeFile('src/schema.graphql', printSchema(schema), () => {
+//     console.log('file saved "src/schema.graphql"');
+//   });
+// }
+// generateSchema();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
